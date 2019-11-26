c0f7189210667038ce06cfeffbd3d7b9e7cf59d9
AlexMatix <alexrodriguezserrano1595@gmail.com>
2019-11-26 17:46:51 -0600

    view clients complete

---
+++ src/app/services/client/client.service.ts
@@ -0,0 +1,67 @@
+import {Injectable} from '@angular/core';
+import {SERVER, PORT, BASE_PATH} from '../../util/const.util';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {ClientModel} from '../../models/client.model';
+import MessagesUtill from '../../util/messages.utill';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ClientService {
+
+    private baseEndpoint = 'client/';
+    private getClientEndpoint = 'active/';
+    private newClientEndpoint = 'new';
+    private headers = new HttpHeaders();
+    private url = SERVER + PORT + BASE_PATH + this.baseEndpoint;
+    private data: any;
+    private firstLoadService = true;
+
+    constructor(
+        private _http: HttpClient
+    ) {
+        this.headers.append('Content-Type', 'application/json');
+        this.headers.append('Access-Control-Allow-Origin', '*');
+    }
+
+    getAllClients() {
+        return this._http.get(
+            this.url + this.getClientEndpoint,
+            {headers: this.headers}
+        );
+    }
+
+    newClient(data) {
+        return this._http.post(
+            this.url + this.newClientEndpoint,
+            data,
+        );
+    }
+
+    deletedClient(id: number) {
+        return this._http.delete(this.url + id, {headers: this.headers});
+    }
+
+    editClient(data: ClientModel) {
+        return this._http.put(this.url, data,
+            {headers: this.headers});
+    }
+
+    getData(callback: any, refresh = false) {
+        if (this.firstLoadService || refresh) {
+            this.getAllClients().subscribe(
+                response => {
+                    callback(response);
+                    this.firstLoadService = false;
+                    this.data = response;
+                },
+                error => {
+                    MessagesUtill.errorMessage('El servicio no esta disponible en este momento');
+                    callback([], true);
+                }
+            );
+        } else {
+            callback(this.data);
+        }
+    }
+}
